b3416eb1eda84843492c947c9af87f160ad4ab38
Zakhar Borisov <zakhar.borisov@mail.ru>
2022-12-26 17:47:30 +0900

    Init commit

---
+++ bank-analyzer/src/main/java/ru/borisov/BankStatementFullParser.java
@@ -0,0 +1,20 @@
+package ru.borisov;
+
+
+public class BankStatementFullParser {
+
+    BankTransaction parseFromCSV(final String line) {
+        // ...
+        throw new UnsupportedOperationException();
+    }
+
+    BankTransaction parseFromJSON(final String line) {
+        // ...
+        throw new UnsupportedOperationException();
+    }
+
+    BankTransaction parseFromXML(final String line) {
+        // ...
+        throw new UnsupportedOperationException();
+    }
+}
